2e29fa2780e94a6978fc2fd3a4893a98015bc8d0
Chloe Warner <cwarner0@byu.edu>
2025-02-25 11:53:23 -0700

    more fixes

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Play } from './play/play';
@@ -6,7 +9,7 @@ import { Scores } from './scores/scores';
 import { About } from './about/about';
 import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
+
 
 function App() {
     const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
