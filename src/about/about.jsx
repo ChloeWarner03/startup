81d207a4ade60210f8f092e8854ce24aa8e261cc
Chloe Warner <cwarner0@byu.edu>
2025-02-25 09:23:52 -0700

    about.jsx changes

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import './about.css';
 
-export function About() {
-  return (
-    <main className="container-fluid text-center">
-    <div id="info" className="quote-box text-light">
-      <p>
-        Whack-a-Mole is a fast paced game where the goal is to whack as many moles as you can before you make too many
-        mistakes and the game ends.
-        BE WARNED: The moles will increase their speed as the game goes on.
-      </p>
+export function About(props) {
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
-      <p>
-        The name Whack-a-Mole is a registered trademark of Mattel. Our use of the name and the game is for non-profit
-        educational use only. No part of this code or program should be used outside of that definition.
-      </p>
-      
-      <p className= "quote"> “The mole is an animal that digs passages searching for the sun. Sometimes he reaches the
-        surface. When he looks at the sun he goes blind.”</p>
-      <p className="author"> Alejandro Jodorowsky</p>
-    </div>
-  </main>
-  );
-}
+  // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  React.useEffect(() => {
+    setImageUrl(`placeholder.jpg`);
+    setQuote('Show me the code');
+    setQuoteAuthor('Linus Torvalds');
+  }, []);
+
+    return (
+      <main className="container-fluid text-center">
+        <div id="info" className="quote-box text-light">
+          <p>
+            Whack-a-Mole is a fast paced game where the goal is to whack as many moles as you can before you make too many
+            mistakes and the game ends.
+            BE WARNED: The moles will increase their speed as the game goes on.
+          </p>
+
+          <p>
+            The name Whack-a-Mole is a registered trademark of Mattel. Our use of the name and the game is for non-profit
+            educational use only. No part of this code or program should be used outside of that definition.
+          </p>
+
+          <p className="quote"> “The mole is an animal that digs passages searching for the sun. Sometimes he reaches the
+            surface. When he looks at the sun he goes blind.”</p>
+          <p className="author"> Alejandro Jodorowsky</p>
+        </div>
+      </main>
+    );
+  }
