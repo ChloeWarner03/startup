5c5085abf3b538b4c5790420945cfeed33e3c3e9
Chloe Warner <cwarner0@byu.edu>
2025-02-25 11:50:15 -0700

    changing the login and the app to get it so then it will display correctly

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
 import { About } from './about/about';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
-export default function App() {
+function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
     return (
         <BrowserRouter>
             <div className='bg-dark text-light' style={{ height: '100vh' }}>
                 <header className="container-fluid">
                     <nav className="navbar navbar-dark">
-                    <img width="55px" src="/molewithcrown.png" style={{ borderRadius: '50%' }} alt="KingMole" />
+                        <img width="55px" src="/molewithcrown.png" style={{ borderRadius: '50%' }} alt="KingMole" />
                         <div className="navbar-brand" href="#">
                             Whack-a-Mole<sup>&reg;</sup>
                         </div>
@@ -24,16 +28,20 @@ export default function App() {
                                     Login
                                 </NavLink>
                             </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='play'>
-                                    GamePlay
-                                </NavLink>
-                            </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='scores'>
-                                    Scores
-                                </NavLink>
-                            </li>
+                            {authState === AuthState.Authenticated && (
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' to='play'>
+                                        GamePlay
+                                    </NavLink>
+                                </li>
+                            )}
+                            {authState === AuthState.Authenticated && (
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' to='scores'>
+                                        Scores
+                                    </NavLink>
+                                </li>
+                            )}
                             <li className='nav-item'>
                                 <NavLink className='nav-link' to='about'>
                                     About Whack-a-Mole
@@ -44,8 +52,21 @@ export default function App() {
                 </header>
 
                 <Routes>
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
+                    <Route
+                        path='/'
+                        element={
+                            <Login
+                                userName={userName}
+                                authState={authState}
+                                onAuthChange={(userName, authState) => {
+                                    setAuthState(authState);
+                                    setUserName(userName);
+                                }}
+                            />
+                        }
+                        exact
+                    />
+                    <Route path='/play' element={<Play userName={userName} />} />
                     <Route path='/scores' element={<Scores />} />
                     <Route path='/about' element={<About />} />
                     <Route path='*' element={<NotFound />} />
@@ -55,15 +76,15 @@ export default function App() {
                     <div className='container-fluid'>
                         <span className='text-reset'>Whack-a-Mole created by Chloe Warner</span>
                         <NavLink className='text-reset' to='https://github.com/ChloeWarner03/startup'>
-                            Github 
+                            Github
                         </NavLink>
                     </div>
                 </footer>
             </div>
         </BrowserRouter>
-  );
+    );
 
-function NotFound() {
-    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
-  }
+    function NotFound() {
+        return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+    }
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,11 +4,12 @@ import { Unauthenticated } from './unauthenticated';
 import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
 
+
 export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg-secondary text-center'>
       <div>
-        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState !== AuthState.Unknown && <h1>Welcome to Whack-a-Mole</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
