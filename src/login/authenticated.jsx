8657b3dd18b9e6d2fd59edaf8dd48a7dde80c514
“Chloe <cwarner0@byu.edu>
2025-03-11 09:47:40 -0600

    authenitcated.jsx cahnges

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-
 import Button from 'react-bootstrap/Button';
 
 import './authenticated.css';
@@ -11,8 +9,16 @@ export function Authenticated(props) {
   const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('userName');
-    props.onLogout();
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+      .catch(() => {
+        // Logout failed. Assuming offline
+      })
+      .finally(() => {
+        localStorage.removeItem('userName');
+        props.onLogout();
+      });
   }
 
   return (
