fb17db3bf2b013eda619d575d600eca37c1cc4e6
“Chloe <cwarner0@byu.edu>
2025-03-27 09:27:30 -0600

    adding peer proxy

---
+++ service/peerProxy.js
@@ -0,0 +1,41 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
+function peerProxy(server) {
+  // Create a websocket object
+  const wss = new WebSocketServer({ server });
+
+  socketServer.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws };
+    connections.push(connection);
+
+    // Forward messages to everyone except the sender
+    socket.on('message', function message(data) {
+      connections.forEach((c) => {
+        if (c.id !== connection.id) {
+          c.ws.send(data);
+        }
+      });
+    });
+
+    // Respond to pong messages by marking the connection alive
+    socket.on('pong', () => {
+      connection.alive = true;
+    });
+  });
+
+  // Keep active connections alive
+  setInterval(() => {
+    connections.forEach((c) => {
+      // Kill any connection that didn't respond to the ping last time
+      if (!c.alive) {
+        c.ws.terminate();
+      } else {
+        c.alive = false;
+        c.ws.ping();
+      }
+    });
+  }, 10000);
+}
+
+module.exports = { peerProxy };
